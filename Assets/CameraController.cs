8383ab170c13ea8763cd84975d8d6ec8584c4384
ryan <ryanhyukkim@gmail.com>
2025-03-11 01:23:35 -0700

    postprocessing

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -12,8 +12,13 @@ public class CameraController : MonoBehaviour
         transform.Translate(0, 0, moveInput);
 
         // 좌우 회전 (A, D 키 또는 마우스)
-        float rotationInput = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
-        transform.Rotate(0, rotationInput, 0);
+        // float rotationInput = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        // transform.Rotate(0, rotationInput, 0);
+        
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        float keyHorizontal = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        float totalRotation = mouseX + keyHorizontal;
+        transform.Rotate(0, totalRotation, 0);
 
         // 상하 회전 (마우스 Y축, 선택 사항)
         float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
